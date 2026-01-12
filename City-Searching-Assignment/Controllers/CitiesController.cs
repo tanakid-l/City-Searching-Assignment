06a1a86ec2a775eb3eaba25211df517ff4388cfa
Tanakid Laoyawira <tanakid.la@gmail.com>
2026-01-12 15:45:11 +0700

    Add/Remove controller
    Add controller for assignment
    Remove example controller which create from editor

---
+++ City-Searching-Assignment/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CitySearchingAssignment.Services;
+
+namespace CitySearchingAssignment.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CitiesController : ControllerBase
+{
+    private readonly ICityService _service;
+    public CitiesController(ICityService service) => _service = service;
+
+    [HttpGet]
+    public IActionResult Get([FromQuery] string name)
+    {
+        if (string.IsNullOrWhiteSpace(name) || name.Length < 2)
+            return BadRequest(new { message = "Query must be at least 2 characters." });
+
+        return Ok(_service.Search(name));
+    }
+}
